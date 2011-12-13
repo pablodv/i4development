60ddfe4b770dff063e6915a97cd0942f0fd90e3e
Pablo <pablo.moreiramora@gmail.com>
2011-12-13 20:54:37 -0300

    generate assets

--- config/application.rb
+++ config/application.rb
@@ -40,8 +40,7 @@ module I4development
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
-    config.assets.paths << Rails.root.join("app", "assets", "web-app-theme")
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
