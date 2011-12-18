d0d9a95d10e137b2fa1b946495ee4794655d9671
Pablo <pablo.moreiramora@gmail.com>
2011-12-18 17:33:12 -0300

    add errors messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def admin_current_tab(resource_name)
     controller.controller_name == resource_name ? 'active' : ''
   end
+
+  def error_message(resource, field)
+    content_tag(:span, resource.errors[field][0], :class => "error") if resource.errors.include?(field)
+  end
+
 end
